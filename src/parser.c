b7f8b93fe7d145e7d16edb036cc2a02530892b06
8dcc <8dcc.git@gmail.com>
2024-04-12 21:07:23 +0200

    Handle TOKEN_QUOTE in parse()

--- src/parser.c
+++ src/parser.c
@@ -9,11 +9,11 @@
 #include "include/parser.h"
 
 Expr* parse(Token* tokens) {
-    Expr* root = NULL;
-    Expr* cur  = NULL;
+    Expr* root     = NULL;
+    Expr* cur      = NULL;
+    bool had_quote = false;
 
-    bool done = false;
-    for (int i = 0; !done; i++) {
+    for (int i = 0;; i++) {
         if (tokens[i].type == TOKEN_EOF || tokens[i].type == TOKEN_LIST_CLOSE) {
             /* NOTE: We don't allocate a NIL Expr, NULL indicates end of lists.
              * NOTE: Since TOKEN_LIST_CLOSE returns NULL, if Expr.children is
@@ -21,10 +21,18 @@ Expr* parse(Token* tokens) {
             if (cur != NULL)
                 cur->next = NULL;
 
-            done = true;
+            /* We reached EOF, we are done. */
             break;
         }
 
+        if (tokens[i].type == TOKEN_QUOTE) {
+            /* For quote tokens, just store it for the next token */
+            had_quote = true;
+            continue;
+        }
+
+        /* If we reached here, we should allocate an expression for this token
+         * type. We will either start or add to the linked list. */
         if (root == NULL) {
             root = malloc(sizeof(Expr));
             cur  = root;
@@ -33,12 +41,20 @@ Expr* parse(Token* tokens) {
             cur       = cur->next;
         }
 
+        /* If the last token was TOKEN_QUOTE, mark it as quoted */
+        if (had_quote) {
+            cur->is_quoted = true;
+            had_quote      = false;
+        }
+
         switch (tokens[i].type) {
             case TOKEN_NUM:
                 cur->type  = EXPR_CONST;
                 cur->val.n = tokens[i].val.n;
                 break;
             case TOKEN_SYMBOL:
+                /* TODO: Don't reuse the string from tokens_scan(), allocate our
+                 * own. Add tokens_free() */
                 cur->type  = EXPR_SYMBOL;
                 cur->val.s = tokens[i].val.s;
                 break;
@@ -58,8 +74,10 @@ Expr* parse(Token* tokens) {
                 }
                 break;
             case TOKEN_EOF:
+            case TOKEN_LIST_CLOSE:
+            case TOKEN_QUOTE:
             default:
-                ERR("Should not reach the EOF case.");
+                ERR("Reached invalid case (Token type %d).", tokens[i].type);
                 return NULL;
         }
     }
