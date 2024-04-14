3e1149768841c7b46a398497073062d5c66429ba
8dcc <8dcc.git@gmail.com>
2024-04-14 16:37:34 +0200

    PRIM_ASSERT -> SL_ASSERT
    Move to util.h

--- src/include/util.h
+++ src/include/util.h
@@ -6,6 +6,15 @@
 /* Wrapper for err_msg() */
 #define ERR(...) err_msg(__func__, __VA_ARGS__)
 
+/* If COND is not true, show error and return NULL */
+#define SL_ASSERT(COND, ...) \
+    if (!(COND)) {           \
+        ERR(__VA_ARGS__);    \
+        return NULL;         \
+    }
+
+/*----------------------------------------------------------------------------*/
+
 /* Print error message to stderr, along with the function name */
 void err_msg(const char* func, const char* fmt, ...);
 
--- src/primitives.c
+++ src/primitives.c
@@ -7,20 +7,13 @@
 #include "include/parser.h"
 #include "include/primitives.h"
 
-/* If COND is not true, show error and return NULL */
-#define PRIM_ASSERT(COND, ...) \
-    if (!(COND)) {             \
-        ERR(__VA_ARGS__);      \
-        return NULL;           \
-    }
-
 Expr* prim_add(Expr* e) {
-    PRIM_ASSERT(e != NULL, "Missing arguments.");
+    SL_ASSERT(e != NULL, "Missing arguments.");
 
     double total = 0;
 
     for (Expr* arg = e; arg != NULL; arg = arg->next) {
-        PRIM_ASSERT(arg->type == EXPR_CONST, "Unexpected argument of type: %d",
+        SL_ASSERT(arg->type == EXPR_CONST, "Unexpected argument of type: %d",
                     arg->type);
 
         total += arg->val.n;
@@ -34,12 +27,12 @@ Expr* prim_add(Expr* e) {
 }
 
 Expr* prim_sub(Expr* e) {
-    PRIM_ASSERT(e != NULL, "Missing arguments.");
+    SL_ASSERT(e != NULL, "Missing arguments.");
 
     double total = e->val.n;
 
     for (Expr* arg = e->next; arg != NULL; arg = arg->next) {
-        PRIM_ASSERT(arg->type == EXPR_CONST, "Unexpected argument of type: %d",
+        SL_ASSERT(arg->type == EXPR_CONST, "Unexpected argument of type: %d",
                     arg->type);
 
         total -= arg->val.n;
@@ -53,12 +46,12 @@ Expr* prim_sub(Expr* e) {
 }
 
 Expr* prim_mul(Expr* e) {
-    PRIM_ASSERT(e != NULL, "Missing arguments.");
+    SL_ASSERT(e != NULL, "Missing arguments.");
 
     double total = 1;
 
     for (Expr* arg = e; arg != NULL; arg = arg->next) {
-        PRIM_ASSERT(arg->type == EXPR_CONST, "Unexpected argument of type: %d",
+        SL_ASSERT(arg->type == EXPR_CONST, "Unexpected argument of type: %d",
                     arg->type);
 
         total *= arg->val.n;
@@ -72,14 +65,14 @@ Expr* prim_mul(Expr* e) {
 }
 
 Expr* prim_div(Expr* e) {
-    PRIM_ASSERT(e != NULL, "Missing arguments.");
+    SL_ASSERT(e != NULL, "Missing arguments.");
 
     double total = e->val.n;
 
     for (Expr* arg = e->next; arg != NULL; arg = arg->next) {
-        PRIM_ASSERT(arg->type == EXPR_CONST, "Unexpected argument of type: %d",
+        SL_ASSERT(arg->type == EXPR_CONST, "Unexpected argument of type: %d",
                     arg->type);
-        PRIM_ASSERT(arg->val.n != 0, "Trying to divide by zero.");
+        SL_ASSERT(arg->val.n != 0, "Trying to divide by zero.");
 
         total /= arg->val.n;
     }
