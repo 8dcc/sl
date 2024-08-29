df3e40179b2c0d3c449a0b7d7bb0fea69ff9dea7
8dcc <8dcc.git@gmail.com>
2024-08-29 02:03:26 +0200

    Add messy version of `defun' to macros.lisp

--- test/macros.lisp
+++ test/macros.lisp
@@ -3,6 +3,7 @@
 ;;   - Macro definitions
 ;;   - Macro expansion
 ;;   - Macro calls
+;;   - Usage of `defmacro' and `defun' macros.
 ;;------------------------------------------------------------------------------
 
 ;; Define a macro called `defmacro'. It will take a `name', a list of `formals'
@@ -36,3 +37,23 @@
 
 ;; Should be equivalent to the previous macro call
 (eval (macroexpand '(inc my-variable)))
+
+;; A messy implementation of `defun'. The whole:
+;;
+;;   (apply begin (quote ...))
+;;
+;; Part is needed because the body is a list of expressions, and we can't pass
+;; that directly to a lambda.
+(defmacro defun (name formals &rest body)
+  (list 'define name
+    (list 'lambda formals
+      (list 'apply 'begin (list 'quote body)))))
+
+;; Define a simple function using the previous `defun' macro. Wrap the call in:
+;;
+;;   (macroexpand 'EXPR)
+;;
+;; To see how the macro expands.
+(defun my-function (a b)
+  (+ a b 10))
+(my-function 1 2)
