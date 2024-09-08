8dcc/sl

commit df957854d8d34e3ea37d7440c668eb96c456cdee

Don't apply function if one of the arguments failed to evaluate

src/eval.c
@@ -125,7 +125,11 @@ Expr* eval(Env* env, Expr* e) {
             /* Evaluate each of the arguments before applying them to the
              * function. Also note that the returned list is allocated, so we
              * will have to free it after calling `apply'. */
-            args = eval_args(env, args);
+            if (args != NULL) {
+                args = eval_args(env, args);
+                if (args == NULL)
+                    return NULL;
+            }
 
             /* Apply the evaluated function to the evaluated argument list */
             Expr* applied = apply(env, func, args);

commit 2b4370321ca9ac25bb8f2e1dc2fdda212623ae94

Fix memory leak when a function argument fails to evaluate
Introduced in df957854d8d34e3ea37d7440c668eb96c456cdee

Again, thanks to valgrind.

src/eval.c
@@ -131,8 +131,10 @@ Expr* eval(Env* env, Expr* e) {
              */
             if (args != NULL) {
                 args = eval_args(env, args);
-                if (args == NULL)
+                if (args == NULL) {
+                    expr_free(func);
                     return NULL;
+                }
             }
 
             /* Apply the evaluated function to the evaluated argument list */