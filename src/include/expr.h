2bd57821a4488722e52b2eb01dd6e56d9026ba76
8dcc <8dcc.git@gmail.com>
2024-09-26 01:29:28 +0200

    Improve comments in expr.h

--- src/include/expr.h
+++ src/include/expr.h
@@ -71,56 +71,95 @@ static inline bool expr_is_applicable(const Expr* e) {
 
 /*----------------------------------------------------------------------------*/
 
-/* Allocate a new empty expression of the specified type */
+/*
+ * Allocate a new empty expression of the specified type. The returned pointer
+ * should be freed by the caller using `expr_free'.
+ */
 Expr* expr_new(enum EExprType type);
 
-/* Free expression and adjacent expressions, along with their children */
-/* TODO: Make separate expr_list_free() function? */
+/*
+ * Free expression, along with their children, if any.
+ *
+ * Doesn't free adjacent expressions (i.e. ignores `e->next'). For freeing a
+ * linked list of expressions, use `expr_list_free'.
+ */
 void expr_free(Expr* e);
 
 /*----------------------------------------------------------------------------*/
 
-/* Allocate a new expr and copy `e'. Needed to avoid double frees and leaks */
+/*
+ * Clone the specified `Expr' structure into an allocated copy, and return it.
+ * The returned pointer must be freed by the caller using `expr_free'.
+ *
+ * In the case of lists, it doesn't clone children. To clone recursively, use
+ * `expr_clone_recur'.
+ *
+ * It also doesn't clone adjacent expressions (i.e. `e->next' is ignored and
+ * `returned->next' is NULL). To clone a list of `Expr' structures, use
+ * `expr_list_clone'.
+ */
 Expr* expr_clone(const Expr* e);
 
-/* Same as `expr_clone', but also clones children recursivelly */
+/*
+ * Same as `expr_clone', but also clones children recursivelly.
+ */
 Expr* expr_clone_recur(const Expr* e);
 
-/* Call `expr_clone_recur' on a linked list of expressions, starting at `e' */
+/*
+ * Clones a linked list of `Expr' structures by calling `expr_clone_recur'.
+ */
 Expr* expr_list_clone(const Expr* e);
 
 /*----------------------------------------------------------------------------*/
 
-/* Print formatted expression */
+/*
+ * Print expression in different formats.
+ */
 void expr_print(const Expr* e);
 void expr_println(const Expr* e);
 void expr_print_debug(const Expr* e);
 
 /*----------------------------------------------------------------------------*/
 
-/* Calculate number of elements in a linked list of Expr structures */
+/*
+ * Count the number of elements in a linked list of `Expr' structures.
+ */
 size_t expr_list_len(const Expr* e);
 
-/* Does the specified linked list contain an expression with the specified
- * type? */
+/*
+ * Does the specified linked list contain an expression with the specified
+ * type?
+ */
 bool expr_list_contains_type(const Expr* e, enum EExprType type);
 
-/* Does the specified linked list ONLY contain expressions with the specified
- * type? */
+/*
+ * Does the specified linked list contain ONLY expressions with the specified
+ * type?
+ */
 bool expr_list_only_contains_type(const Expr* e, enum EExprType type);
 
-/* Does the specified linked list contain only numeric types? */
+/*
+ * Does the specified linked list contain ONLY expressions with numeric types?
+ * Uses `expr_is_number'.
+ */
 bool expr_list_only_contains_numbers(const Expr* e);
 
 /*----------------------------------------------------------------------------*/
 
-/* Is the specified expression an empty list? */
+/*
+ * Is the specified expression an empty list? Note that the empty list is also
+ * used to represent false in functions that return predicates.
+ */
 bool expr_is_nil(const Expr* e);
 
-/* Return true if `a' and `b' have the same effective value */
+/*
+ * Return true if `a' and `b' have the same effective value.
+ */
 bool expr_equal(const Expr* a, const Expr* b);
 
-/* Return true if `a' is lesser/greater than `b' */
+/*
+ * Return true if `a' is lesser/greater than `b'.
+ */
 bool expr_lt(const Expr* a, const Expr* b);
 bool expr_gt(const Expr* a, const Expr* b);
 
